336b17f0db4e940ce52e9bf44294a74133d1fc60
ssdamouni <66917932+ssdamouni@users.noreply.github.com>
2021-03-23 10:42:23 -0700

    fixed datetimelocal issue

--- forms.py
+++ forms.py
@@ -32,7 +32,7 @@ class EventForm(FlaskForm):
     title = StringField("What is the event called", validators=[InputRequired()])
     description = StringField("Description", validators=[InputRequired()])
     address = StringField("What is the Address of the event?", validators=[InputRequired()])
-    date = DateTimeLocalField("Date of Event", validators=[InputRequired()])
+    date = DateTimeLocalField("Date of Event", format='%Y-%m-%dT%H:%M', validators=[InputRequired()])
     region_id = SelectField("Where is this job?", coerce=int, validators=[InputRequired()])
     genre = StringField("What type of music will you play?", validators=[InputRequired()])
 
@@ -41,7 +41,7 @@ class JobForm(FlaskForm):
     title = StringField("What is the event called", validators=[InputRequired()])
     description = StringField("Description", validators=[InputRequired()])
     pay = FloatField("How does the gig pay(in dollars)", validators=[InputRequired()])
-    date = DateTimeLocalField("Date of Event", validators=[InputRequired()]) 
+    date = DateTimeLocalField("Date of Event", format='%Y-%m-%dT%H:%M', validators=[InputRequired()]) 
     region_id = SelectField("Where is this job?", coerce=int, validators=[InputRequired()])
     genre = StringField("What type of music will you play?", validators=[InputRequired()])
 
