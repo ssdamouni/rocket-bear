ca941ef1c1a07418f64888be4b9cf1ef0669a85d
ssdamouni <66917932+ssdamouni@users.noreply.github.com>
2021-04-12 10:00:43 -0700

    adding jobs to profile

--- app.py
+++ app.py
@@ -190,6 +190,7 @@ def user_profile(user_id):
                 genres_list.append(genres[k].genre)
                 k += 1
         events = EventPost.query.filter_by(user_id=user_id)
+        jobs = JobPost.query.filter_by(user_id=user_id)
         #get a list of piece ids
         pieces = UserPiece.query.filter_by(user_id=user_id)
         id_list = []
@@ -200,7 +201,7 @@ def user_profile(user_id):
         id_list = str(id_list)
         pieces_resp = requests.get(f"https://api.openopus.org/work/list/ids/{id_list[1:-1]}.json")
         user_pieces = pieces_resp.json()
-        return render_template('users/profile.html', user=user, events=events, id_list=id_list_str, works=user_pieces, instruments=instruments_list, roles=roles_list, genres=genres_list)
+        return render_template('users/profile.html', user=user, events=events, jobs=jobs, id_list=id_list_str, works=user_pieces, instruments=instruments_list, roles=roles_list, genres=genres_list)
     else:
         return render_template('home-anon.html')
 
